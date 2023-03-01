c0fd16322b2fde885643d8ef4d18ad93dfa609d9
William Yang <wy2037@nyu.edu>
2023-03-01 13:55:11 -0500

    Updated Barriers

--- Rhythm Game/Assets/Scripts/Bounce.cs
+++ Rhythm Game/Assets/Scripts/Bounce.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Bounce : MonoBehaviour
 {
     public Player player;
-    public float force;
+    public float force, fadeSpeed;
+    public SpriteRenderer sprite;
+    float fade;
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             player.canMove = false;
+            sprite.color = new Color(1, 1, 1, 1);
             //Invoke("CanMoveAgain", bounceTime);
             if (player.isGrounded) {
                 other.GetComponent<Rigidbody2D>().AddForce(new Vector2(force, player.jumpForce/3f));
             } else {
                 other.GetComponent<Rigidbody2D>().AddForce(new Vector2(force, player.jumpForce));
             }
+        }
+    }
+
+    void Update() {
+        float alpha = sprite.color.a;
+        if (sprite.color.a > 0) {
+            alpha -= Time.deltaTime * fadeSpeed/255;
+            sprite.color = new Color(1, 1, 1, alpha);
         }
     }
 }
