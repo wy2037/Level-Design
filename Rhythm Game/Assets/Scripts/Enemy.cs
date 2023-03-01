d23ff97718314654363e09aca448ba19a0748768
William Yang <wy2037@nyu.edu>
2023-03-01 16:25:18 -0500

    fixes

--- Rhythm Game/Assets/Scripts/Enemy.cs
+++ Rhythm Game/Assets/Scripts/Enemy.cs
@@ -60,4 +60,10 @@ public class Enemy : MonoBehaviour
         yield return new WaitForSeconds(0.6f);
         MusicHandler.m.addObserver(gameObject);
     }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            gameObject.SetActive(false);
+        }
+    }
 }
